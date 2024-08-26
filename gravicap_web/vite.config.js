ad6db578397aaa0d47a5b37898e879e98a5ec79f
marianobianqui <uprisingmjb@gmail.com>
2024-08-26 14:52:38 -0300

    feat: Configuracion para build en github pages

--- gravicap_web/vite.config.js
+++ gravicap_web/vite.config.js
@@ -8,9 +8,13 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: "/gravicap/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: true
   }
 })
