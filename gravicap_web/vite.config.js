a94b4caed17644c9569ae9362ce57368cd4d495e
marianobianqui <83589062+marianobianqui@users.noreply.github.com>
2024-09-06 14:42:17 -0300

    Update vite.config.js

--- gravicap_web/vite.config.js
+++ gravicap_web/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: 'https://gravicap.vercel.app/home' 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
