ba77fa64e86bd1998023068a0c4c2c44e7d72a17
marianobianqui <83589062+marianobianqui@users.noreply.github.com>
2024-09-23 14:49:29 -0300

    Update HomeView.vue

--- gravicap_web/src/views/HomeView.vue
+++ gravicap_web/src/views/HomeView.vue
@@ -288,7 +288,7 @@ h3 {
     .grid-izquierda {
         margin-left: 10vw;
         height: 30vh;
-        margin-bottom: 17vh;
+        margin-bottom: 8vh;
     }
     .grid-derecha {
         margin-left: 10vw;
