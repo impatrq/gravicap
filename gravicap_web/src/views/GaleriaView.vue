e17f43c8a783e6c615ad769372d01db790cb7935
marianobianqui <83589062+marianobianqui@users.noreply.github.com>
2024-10-03 16:15:50 -0300

    Update GaleriaView.vue

--- gravicap_web/src/views/GaleriaView.vue
+++ gravicap_web/src/views/GaleriaView.vue
@@ -10,11 +10,11 @@
             </div>
             <div class="grid_3">
                 <p>Estructura sin pintar</p>
-            <img class="imagenes" src="../../public/estructuraSinPintar.png">
+            <img class="imagenes" src="../../public/estructuraSinPintar (1).png">
             </div>
             <div class="grid_4">
                 <p>Estructura pintada</p>
-            <img class="imagenes" src="../../public/estructuraPintada (1).png">
+            <img class="imagenes" src="../../public/estructuraPintada (2).png">
             </div>
             <div class="grid_5">
                 <p>Visita a Radio Vox</p>
