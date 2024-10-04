983857d5466a8dfc1f1d4606f6f41ebf63bd79d5
marianobianqui <83589062+marianobianqui@users.noreply.github.com>
2024-10-04 08:12:30 -0300

    Update GaleriaView.vue

--- gravicap_web/src/views/GaleriaView.vue
+++ gravicap_web/src/views/GaleriaView.vue
@@ -168,7 +168,7 @@ p {
     }
     .grid_1 {
                 .imagenes {
-                            margin-bottom: 2vh;
+                            margin-bottom: 2vw;
                 }
     }
 }
