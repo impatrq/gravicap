ffd0abf36a368811d29b0dc53cb159f610889b2d
marianobianqui <83589062+marianobianqui@users.noreply.github.com>
2024-10-04 12:21:51 -0300

    Update NosotrosView.vue

--- gravicap_web/src/views/NosotrosView.vue
+++ gravicap_web/src/views/NosotrosView.vue
@@ -8,9 +8,9 @@
                 <h2>Integrantes</h2>
                 <div class="presentacion-integrantes">
                     <div class="integrante"></br>Tomás - Aplicación<img class="yatu" src="../../public/tomas.png"></div>
-                    <div class="integrante"></br>Fausto - Rezar<img class="yatu" src="../../public/fausto.png"></div>
+                    <div class="integrante"></br>Fausto - Electrónica<img class="yatu" src="../../public/fausto.png"></div>
                     <div class="integrante"></br>Augusto - Estructura<img class="yatu" src="../../public/augusto.png"></div>
-                    <div class="integrante"></br>Tatiana - Electrónica<img class="yatu" src="../../public/tatiana.png"></div>
+                    <div class="integrante"></br>Tatiana - Programación<img class="yatu" src="../../public/tatiana.png"></div>
                     <div class="integrante"></br>Gabriel - Redes<img class="yatu" src="../../public/gabriel.png"></div>
                     <div class="integrante"></br>Mariano - Página Web<img class="yatu" src="../../public/mariano.png"></div>
                 </div>
