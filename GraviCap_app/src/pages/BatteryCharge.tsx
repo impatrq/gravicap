18eada7cc9f18f9dfdd983691b3c684b1d630299
Tomi800 <tomascalleja918@gmail.com>
2024-11-16 11:49:27 -0300

    feat: primera muestra de datos con el server

--- GraviCap_app/src/pages/BatteryCharge.tsx
+++ GraviCap_app/src/pages/BatteryCharge.tsx
@@ -1,8 +1,36 @@
 import { IonBackButton, IonButtons, IonCard, IonContent, IonHeader, IonToolbar } from '@ionic/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./CSS BatteryCharge.css"
 
 function BatteryCharge () {
+
+  const [data, setData] = useState({
+    carga: null,
+    nombre: '',
+    corriente: null,
+    voltage: null,
+    potencia: null
+  });
+
+  useEffect(() => {
+    // Realizamos el fetch para obtener los datos
+    fetch('http://192.168.124.79/sensor?nombre=Sensor_0x40') // Cambia la URL a la IP del ESP8266
+      .then((response) => response.json())
+      .then((data) => {
+        // Actualizamos el estado con los valores recibidos
+        setData({
+          carga: data.carga,
+          nombre: data.nombre,
+          corriente: data.corriente,
+          voltage: data.voltage,
+          potencia: data.potencia
+        });
+      })
+      .catch((error) => {
+        console.error('Error al obtener los datos:', error);
+      });
+  }, []); // El array vacío asegura que solo se ejecute una vez al montar el componente
+
   return (
     <>
       {/*header principal de la pantalla*/}
@@ -27,7 +55,7 @@ function BatteryCharge () {
                   {/*carta circular delantera contendora del valor porcentaul de la carga de batería*/}
                   <IonCard>
                     {/*valor porcentual de la carga de la batería*/}
-                    <strong><p>73%</p></strong>
+                    <strong><p>{data.carga} % </p></strong>
                   </IonCard>
                 </div>
               </IonCard>
