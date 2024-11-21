b31a3580b2dc753303cb663ba07a4742d857fe21
Fausto <83584094+Fausto-AM@users.noreply.github.com>
2024-11-21 10:37:24 -0300

    Delete Archivo Viejo

--- Documentación/Carpeta Técnica GraviCap/adicional2.c
+++
@@ -1,8 +0,0 @@
-typedef struct {
-                    i2c_inst_t *i2c;
-                    uint8_t addr;
-                    uint8_t sda_pin;
-                    uint8_t scl_pin;
-                    uint32_t clock;
-                    float _current_lsb;
-                } ina219_t;
